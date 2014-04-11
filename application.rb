ca23e14e1c4364fc592a0cd7b6929d7b2271396a
Roberto Dip <dip.jesusr@gmail.com>
2014-04-10 22:59:34 -0300

    Updates the groff_parser gem

--- application.rb
+++ application.rb
@@ -3,6 +3,7 @@ require "patron"
 require "elasticsearch"
 require "lotus-controller"
 require "lotus-view"
+require "groff_parser"
 
 # Require all application files.
 Dir["./app/controllers/**/*.rb"].each  { |rb| require rb }
