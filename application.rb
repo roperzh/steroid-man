7a072aa92c51eec8d505b6de9633148e03a3d2ad
Roberto Dip <dip.jesusr@gmail.com>
2014-05-04 19:22:35 -0400

    Adds an application layout to hold all the views

--- application.rb
+++ application.rb
@@ -4,9 +4,11 @@ require "elasticsearch"
 require "lotus-controller"
 require "lotus-view"
 require "groff_parser"
+require "sass"
 
 # Require all application files.
 Dir["./app/controllers/**/*.rb"].each  { |rb| require rb }
+Dir["./app/layouts/**/*.rb"].each      { |rb| require rb }
 Dir["./app/views/**/*.rb"].each        { |rb| require rb }
 Dir["./app/services/**/*.rb"].each     { |rb| require rb }
 
