3a2bfc78b9c88bf3365c7ade4d7f365895f774ff
Roberto Dip <dip.jesusr@gmail.com>
2014-05-14 00:42:45 -0300

    Improves the presentation of the results, showing the correct information and setting a max number of characters for the description

--- app/presenters/package_presenter.rb
+++ app/presenters/package_presenter.rb
@@ -5,4 +5,17 @@ class PackagePresenter
     raw_attribute = @object["_source"][attribute.to_s]
     raw_attribute
   end
+
+  def short_description
+    truncate get(:description)
+  end
+
+  private
+
+  def truncate(input, limit = 139)
+    return unless input
+
+    delimiter = input.size > limit ? "..." : ""
+    "#{input[0..limit]}#{delimiter}"
+  end
 end
